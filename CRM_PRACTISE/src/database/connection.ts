51a6e6a85fcb7025c57298aabf85a0e10fab2c74
farzanhossan <farzanhossans@gmail.com>
2019-12-08 11:45:35 +0600

    Connection Update

--- CRM_PRACTISE/src/database/connection.ts
+++ CRM_PRACTISE/src/database/connection.ts
@@ -5,5 +5,13 @@ const sequelize = new Sequelize('crm_db', 'root', 'Admin@123',{
     dialect: 'mysql',
     operationAliases: false
 })
+sequelize
+  .authenticate()
+  .then(() => {
+    console.error('Database Connected');
+  })
+  .catch((err: any) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 export default sequelize;
